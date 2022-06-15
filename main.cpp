30269cb2051699f20daaca25b33fa356d9efb0e8
david9472 <44641332+david9472@users.noreply.github.com>
2022-06-15 18:52:53 +0200

    corrected file path for new project

--- main.cpp
+++ main.cpp
@@ -5,31 +5,21 @@
 int main(int argc, char *argv[])
 {
   analytical_engine engine;
-    engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                       "AI/MISC/Klausur_1/tfidf/cmake-build-debug/musk1.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/musk2.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/musk3.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/musk4.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/musk5.txt");
+  engine.addDocument("musk1.txt");
+  engine.addDocument("musk2.txt");
+  engine.addDocument("musk3.txt");
+  engine.addDocument("musk4.txt");
+  engine.addDocument("musk5.txt");
 
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/trump1.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/trump2.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/trump3.txt");
-  engine.addDocument("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                     "AI/MISC/Klausur_1/tfidf/cmake-build-debug/trump4.txt");
+  engine.addDocument("trump1.txt");
+  engine.addDocument("trump2.txt");
+  engine.addDocument("trump3.txt");
+  engine.addDocument("trump4.txt");
 
   engine.setUpData();
   engine.createTermFrequenciesAndTFIDF();
 
-  engine.setQuery("/Users/davidfruhwirth/Documents/Uni/2_Semester/Introduction DS and "
-                  "AI/MISC/Klausur_1/tfidf/cmake-build-debug/trump5.txt");
+  engine.setQuery("trump5.txt");
 
   for (const auto& file : engine.parsed_files_)
     std::cout << file << std::endl;
